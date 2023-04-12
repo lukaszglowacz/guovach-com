cd9830ea894c36c0f8703cbe92b5a99b68033b33
lukaszglowacz <>
2023-04-12 16:28:44 +0000

    fix small bugs, style buttons and add links to a elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,6 +134,12 @@ btnsCategory.forEach(btn => {
         // get category
         const category = btn.getAttribute('data-category');
 
+        // change font weight of active button
+        btnsCategory.forEach(btn => {
+            btn.style.fontWeight = 'normal';
+        });
+        btn.style.fontWeight = '600';
+
         // show images that match the category or show all images
         images.forEach(img => {
             if (category === 'all' || img.getAttribute('data-category').includes(category)) {
@@ -153,18 +159,30 @@ document.querySelector('.btn-category[data-category="all"]').click();
 const categories = document.querySelectorAll('.projects-btn-category');
 const projects = document.querySelectorAll('.project');
 
+// show all projects by default
+projects.forEach(project => {
+    project.style.display = 'block';
+});
+
+// Add eventlistener
 categories.forEach(category => {
     category.addEventListener('click', () => {
         // remove active class from all categories
         categories.forEach(cat => cat.classList.remove('active'));
         // add active class to clicked category
         category.classList.add('active');
-        
+
+        // change font weight of active category
+        categories.forEach(cat => {
+            cat.style.fontWeight = 'normal';
+        });
+        category.style.fontWeight = '600';
+
         const categoryValue = category.getAttribute('data-category');
-        
+
         projects.forEach(project => {
             const projectCategory = project.getAttribute('data-category');
-            
+
             if (categoryValue === 'all' || categoryValue === projectCategory) {
                 project.style.display = 'block';
             } else {
@@ -174,9 +192,10 @@ categories.forEach(category => {
     });
 });
 
+// simulate a click on the "All" button to show all images by default
+document.querySelector('.projects-btn-category[data-category="all"]').click();
 
 // Form element synchronized with Google Sheets document via Google Apps Script
-
 window.addEventListener("load", function() {
     const form = document.getElementById('form');
     form.addEventListener("submit", function(e) {
@@ -193,12 +212,13 @@ window.addEventListener("load", function() {
         // Display overlay and thank-you-message
         const overlay = document.querySelector('.overlay');
         overlay.style.display = 'block';
-
         const thankYouMessage = document.querySelector('.thank-you-message');
         thankYouMessage.style.display = 'flex';
       })
     });
   });
+
+
 
 //   Close thank you screen after registering form button
 function handleCloseButtonClick() {
@@ -213,3 +233,33 @@ function handleCloseButtonClick() {
   }
   
   handleCloseButtonClick();
+
+//   When user sroll page navbar list become active on section user actually is
+function setActiveNavItem() {
+    const navItems = document.querySelectorAll('.nav-item');
+    let activeItem = navItems[0]; // Set the first item as the active item
+    
+    // Listen for the scroll event
+    window.addEventListener('scroll', () => {
+      // Get the current scroll position
+      const scrollPos = window.scrollY;
+      
+      // Check each section's position to see which one is currently in view
+      navItems.forEach(item => {
+        const sectionId = item.getAttribute('data-section-id');
+        const section = document.getElementById(sectionId);
+        const sectionPos = section.getBoundingClientRect().top + scrollPos;
+        
+        if (scrollPos >= sectionPos && scrollPos < sectionPos + section.offsetHeight) {
+          // Add the active class to the corresponding navigation item
+          if (item !== activeItem) {
+            activeItem.classList.remove('active');
+            item.classList.add('active');
+            activeItem = item;
+          }
+        }
+      });
+    });
+  }
+  
+  setActiveNavItem();
